f8e5bbeddad41536a592074bcaefcd83d62281c7
Wxrinsodel <pinpinahgase@gmail.com>
2024-09-10 19:39:33 +0700

    idk

--- Day 2.py
+++ Day 2.py
@@ -1,1 +1,16 @@
-print("Hello world")
+print("hello")
+print("HELLO WORLD", "Deep" , sep="\n") #, sep="\n" คือการทำให้ตัวด้านหน้าคำสั่งขึ้นบรรทัดใหม่
+#Lecture
+
+"""
+Long comment
+"""
+
+"""
+Data type
+1. String (text)
+2. Integer (1,2,3,67,2983, -1)
+3. floating (1.98)
+4. Boolean (yes/no)
+5. Nontype ()
+"""
