917a0dd6bb989c80f5fa7c2e59edbad184b197c5
Wxrinsodel <pinpinahgase@gmail.com>
2024-09-10 19:53:56 +0700

    final 10/09

--- Day 2.py
+++ Day 2.py
@@ -1,9 +1,13 @@
 print("hello")
 print("HELLO WORLD", "Deep" , sep="\n") #, sep="\n" คือการทำให้ตัวด้านหน้าคำสั่งขึ้นบรรทัดใหม่
+
+
 #Lecture
 
 """
+
 Long comment
+
 """
 
 """
@@ -12,5 +16,16 @@ Data type
 2. Integer (1,2,3,67,2983, -1)
 3. floating (1.98)
 4. Boolean (yes/no)
-5. Nontype ()
+5. Nontype ( nothing )
+
 """
+
+# for data type : DONT FOR GET TO TYPE """PRINT"""
+print(type(0.89))
+
+# if we want to check if the first one is correct for the second one like:
+
+print(isinstance(True,int))
+
+#True == 1
+#False == 0
